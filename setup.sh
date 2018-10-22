dadaa840b06c4df23ad1840553a57ff609a98962
Jeffrey Kranenburg <jeff.kranenburg@gmail.com>
2018-10-23 12:29:50 +1300

    use the force

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 ../.travis/deploy # this key should have push access
 ssh-add ../.travis/deploy
 git remote add deploy administrator@tga.ecti.ac.nz:/home/docker/WEBCONTENT/beta/repo
-git push --set-upstream deploy master
+git push --set-upstream deploy master --force
