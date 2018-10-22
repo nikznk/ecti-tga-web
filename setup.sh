4a38c4ae973ef4e3f1b48836c207c8750b0b27e1
Jeffrey Kranenburg <jeff.kranenburg@gmail.com>
2018-10-23 12:20:23 +1300

    Moved stuff into script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+
+git init
+ls -la
+git branch
+git checkout master
+eval "$(ssh-agent -s)" #start the ssh agent
+chmod 600 ../.travis/deploy # this key should have push access
+ssh-add ../.travis/deploy
+git remote add deploy administrator@tga.ecti.ac.nz:/home/docker/WEBCONTENT/beta/repo
+git push deploy
