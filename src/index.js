1b9c92d44e5c2ed197be264b69930a57e0f23ed9
Jeffrey Kranenburg <jeff.kranenburg@gmail.com>
2018-11-12 20:19:56 +1300

    Update travis to build-js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import "./index.css";
 import "./tailwind.css";
 import "./App.css";
 import "jquery";
-import App from "./App";
 import "lightbox2";
 import "../node_modules/lightbox2/dist/css/lightbox.css";
 import registerServiceWorker from "./registerServiceWorker";
