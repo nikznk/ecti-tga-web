28b51a7f0cb1ba47d40f7fdfdb6c779bf37722ca
Nikhil K <nikz.kamble@hotmail.com>
2018-11-12 13:32:05 +1300

    changed ssh creds

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ git checkout master
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 ../.travis/deploy # this key should have push access
 ssh-add ../.travis/deploy
-git remote add deploy administrator@tga.ecti.ac.nz:/home/docker/WEBCONTENT/beta/repo
+git remote add deploy docker@tga.ecti.ac.nz:/home/docker/WEBCONTENT/beta/repo
 git push --set-upstream deploy master --force
